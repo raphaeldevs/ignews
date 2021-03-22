95bb3526c8e32df935e5aa8f44c165bfe1976dcb
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-22 17:39:53 -0300

    feat: styling home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,26 @@
 import Head from 'next/head'
 
+import styles from '../styles/home.module.scss'
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1>
-        Hi, Raphael!
-      </h1>
+      <main className={styles.contentContainer}>
+        <section  className={styles.hero}>
+          <span>👋 Hey, welcome!</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get acess to all publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
     </>
   )
 }
