b6b683b04d3e7b3f4605c76c84f3cb27489ddd65
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-22 16:59:46 -0300

    fixup! set favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
 
-          <link rel="shortcut icon" href="favicon.png" type="image/png"/>
+          <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
         </Head>
 
         <body>
