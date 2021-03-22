9e9a0cc1faf6de8b39b806fa07076894badd2e6f
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-22 12:24:13 -0300

    add fonts and remove title warning

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,8 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-          <title>ig.news</title>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
         </Head>
 
         <body>
