07188e9e476eb1007d580cff73adea2c997fb2cc
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-23 17:56:39 -0300

    handle redirect user to checkout payment

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,43 @@
 import styles from './styles.module.scss'
 
+import { signIn, useSession } from 'next-auth/client'
+
+import { getStripeJs } from '../../services/stripe-js'
+import { api } from '../../services/api'
+
 interface SubscribeButtonProps {
   priceId: string
 }
 
 export default function SubscribeButton({}: SubscribeButtonProps) {
+  const [session] = useSession()
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe Now
     </button>
   )
