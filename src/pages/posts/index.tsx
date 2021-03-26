9c60ddaf068239a6a22928979efdf252c878945e
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-25 22:53:53 -0300

    list posts from prismic api

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,11 +3,24 @@ import Head from 'next/head'
 
 import Prismic from '@prismicio/client'
 
+import { RichText } from 'prismic-dom'
+
 import { getPrismicClient } from '../../services/prismic'
 
 import styles from './styles.module.scss'
 
-export default function Posts() {
+type Post = {
+  slug: string
+  title: string
+  excerpt: string
+  updatedAt: string
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -16,26 +29,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>25 de março de 2021</time>
-            <strong>Is Java still worth learning?</strong>
-            <p>Learning to program has become a differentiator, many schools and teaching centers are already adopting the discipline in their curriculum, however it is necessary ...</p>
-          </a>
-          <a href="#">
-            <time>25 de março de 2021</time>
-            <strong>Is Java still worth learning?</strong>
-            <p>Learning to program has become a differentiator, many schools and teaching centers are already adopting the discipline in their curriculum, however it is necessary ...</p>
-          </a>
-          <a href="#">
-            <time>25 de março de 2021</time>
-            <strong>Is Java still worth learning?</strong>
-            <p>Learning to program has become a differentiator, many schools and teaching centers are already adopting the discipline in their curriculum, however it is necessary ...</p>
-          </a>
-          <a href="#">
-            <time>25 de março de 2021</time>
-            <strong>Is Java still worth learning?</strong>
-            <p>Learning to program has become a differentiator, many schools and teaching centers are already adopting the discipline in their curriculum, however it is necessary ...</p>
-          </a>
+          {posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          )) }
         </div>
       </main>
     </>
@@ -52,9 +52,20 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100
   })
 
-  console.log(JSON.stringify(response, null, 2))
+  const posts = response.results.map(post => ({
+    slug: post.uid,
+    title: RichText.asText(post.data.title),
+    excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+    updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    })
+  }))
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
