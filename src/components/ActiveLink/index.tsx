1aba62a5d1cc20248ccf170572e1e1df2a21a509
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-25 23:32:14 -0300

    create ActiveLink component

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,23 @@
+import { cloneElement, ReactElement } from 'react'
+
+import Link, { LinkProps } from 'next/link'
+import { useRouter } from 'next/router'
+
+interface ActiveLinkProps extends LinkProps {
+  children: ReactElement,
+  activeClassName: string
+}
+
+export function ActiveLink({ activeClassName, children, ...rest }: ActiveLinkProps) {
+  const { asPath } = useRouter()
+
+  const className = asPath === rest.href
+    ? activeClassName
+    : ''
+
+  return (
+    <Link {...rest}>
+      { cloneElement(children, { className }) }
+    </Link>
+  )
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { ActiveLink } from '../ActiveLink'
 import SignInButton from '../SignInButton'
 
 import styles from './styles.module.scss'
@@ -9,8 +10,13 @@ export default function Header() {
         <img src="/images/logo.svg" alt="ig.news" />
 
         <nav>
-          <a href="#" className={styles.active}>Home</a>
-          <a href="#" >Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
