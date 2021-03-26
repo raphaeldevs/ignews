eaec6229cfae4a5d50882bf96f260c37ed2f704b
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-26 01:16:02 -0300

    redirect user for post page if has a active subscription

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,16 +1,26 @@
-import styles from './styles.module.scss'
+import { useRouter } from 'next/router'
 
 import { signIn, useSession } from 'next-auth/client'
 
 import { getStripeJs } from '../../services/stripe-js'
 import { api } from '../../services/api'
 
+import styles from './styles.module.scss'
+
 export default function SubscribeButton() {
   const [session] = useSession()
+  
+  const router = useRouter()
 
   async function handleSubscribe() {
     if (!session) {
       signIn('github')
+
+      return
+    }
+
+    if (session.activeSubscription) {
+      router.push('/posts')
 
       return
     }
